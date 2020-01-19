b6bfc092768d8c676a750a2c1e12b63ac3f7d780
Arundeep Singh Chohan <arundeepchohan2009@hotmail.com>
2020-01-18 22:05:09 -0800

    Update LangaraLogin.java

--- Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
+++ Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
@@ -22,12 +22,11 @@ public class LangaraLogin {
 	@BeforeMethod
 	public void BeforeMethod()
 	{
-		//System.setProperty("webdriver.ie.driver", "C:\\ie\\IEDriverServer_Win32_3.150.1\\IEDriverServer.exe");
-		//WebDriver driver = new InternetExplorerDriver();
-		System.setProperty("webdriver.chrome.driver", "C:\\chrome\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		String baseurl = "https://swing.langara.bc.ca/prod/twbkwbis.P_WWWLogin";
+	    //System.setProperty("webdriver.ie.driver", "C:\\ie\\IEDriverServer_Win32_3.150.1\\IEDriverServer.exe");		//WebDriver driver = new InternetExplorerDriver();
+	    System.setProperty("webdriver.chrome.driver", "C:\\chrome\\chromedriver_win32\\chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    String baseurl = "https://swing.langara.bc.ca/prod/twbkwbis.P_WWWLogin";
 	    driver.get(baseurl);
 	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	}
@@ -39,29 +38,29 @@ public class LangaraLogin {
 	    String actualUrl,expectedUrl;
 	    WebElement username,password,submitBtn;
 	    wait = new WebDriverWait(driver,2);
-		//<input type="text" name="sid" size="11" maxlength="9" id="UserID">
+            //<input type="text" name="sid" size="11" maxlength="9" id="UserID">
 	    username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
-		//Instead of read just enter your username and password
-		userName = "100201711";
-		username.sendKeys(userName);
-		//<input type="password" name="PIN" size="31" maxlength="30">
-		password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("PIN")));
-		passWord = "Ac-19920330";
-		password.sendKeys(passWord);
-		//<input type="submit" value="Login">	    
-		submitBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='submit']")));;
-		submitBtn.click();
-		//Added a bit of checking in case you entered the wrong values
-		actualUrl ="https://swing.langara.bc.ca/prod/twbkwbis.P_GenMenu?"; 
-		expectedUrl = driver.getCurrentUrl();
-		Assert.assertTrue(expectedUrl.contains(actualUrl));
+	    //Instead of read just enter your username and password
+	    userName = "";
+	    username.sendKeys(userName);
+	    //<input type="password" name="PIN" size="31" maxlength="30">
+	    password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("PIN")));
+	    passWord = "";
+	    password.sendKeys(passWord);
+	    //<input type="submit" value="Login">	    
+	    submitBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='submit']")));;
+	    submitBtn.click();
+	    //Added a bit of checking in case you entered the wrong values
+            actualUrl ="https://swing.langara.bc.ca/prod/twbkwbis.P_GenMenu?"; 
+	    expectedUrl = driver.getCurrentUrl();
+	    Assert.assertTrue(expectedUrl.contains(actualUrl));
 	}
 
 	@AfterMethod
 	public void aftermethod()
 	{
-		wait = new WebDriverWait(driver,2);
-		WebElement students = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Students")));
+	    wait = new WebDriverWait(driver,2);
+	    WebElement students = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Students")));
 	    //<a href="/prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu" onmouseover="window.status='Students'; return true" onmouseout="window.status=''; return true" onfocus="window.status='Students'; return true" onblur="window.status=''; return true">Students</a>
 	    students.click();
 	    WebElement studentrecords = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
@@ -75,13 +74,12 @@ public class LangaraLogin {
 	    //<td class="dddefault">CPSC</td>
 	    List<WebElement> courses = driver.findElements(By.className("dddefault"));
 	    Iterator<WebElement> iter = courses.iterator();
-		// This will check whether list has some element or not
-		while (iter.hasNext()) {
-			WebElement item = iter.next();
-			String label = item.getText();
-			System.out.println(label);
-		}
-		driver.quit();
-		
+	    // This will check whether list has some element or not
+	    while (iter.hasNext()) {
+		WebElement item = iter.next();
+		String label = item.getText();
+		System.out.println(label);
+	    }
+	    driver.quit();	
 	}
 }
