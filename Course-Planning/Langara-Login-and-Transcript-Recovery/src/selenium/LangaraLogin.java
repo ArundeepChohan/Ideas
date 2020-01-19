87656a322086798b601b6b53c51d3959062adc74
Arundeep Singh Chohan <arundeepchohan2009@hotmail.com>
2020-01-18 19:26:09 -0800

    Add files via upload

---
+++ Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
@@ -0,0 +1,92 @@
+package selenium;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LangaraLogin {
+
+	WebDriver driver = null;
+	WebDriverWait wait = null;
+	
+	@BeforeMethod
+	public void BeforeMethod()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\chrome\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		String baseurl = "https://swing.langara.bc.ca/prod/twbkwbis.P_WWWLogin";
+	    driver.get(baseurl);
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+	    Login();
+	    wait = new WebDriverWait(driver,5);
+	    wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Students")));
+	    //<a href="/prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu" onmouseover="window.status='Students'; return true" onmouseout="window.status=''; return true" onfocus="window.status='Students'; return true" onblur="window.status=''; return true">Students</a>
+	    WebElement students = driver.findElement(By.linkText("Students"));
+	    students.click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
+	    WebElement studentrecords = driver.findElement(By.linkText("Student Records"));
+	    studentrecords.click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Your Transcript")));
+	    WebElement viewyourtranscript = driver.findElement(By.linkText("View Your Transcript"));
+	    viewyourtranscript.click();
+	    ///html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody
+	    //WebElement transcript = driver.findElement(By.xpath("/html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody"));
+	    //System.out.println(transcript.getText());
+	    //Would like to pull out the information and store it into a class with Subject, No, Title, Credit, Grade, GPA. Remove the last column as well.
+	    //<td class="dddefault">CPSC</td>
+	    List<WebElement> courses = driver.findElements(By.className("dddefault"));
+	    Iterator<WebElement> iter = courses.iterator();
+		// This will check whether list has some element or not
+		while (iter.hasNext()) {
+			WebElement item = iter.next();
+			String label = item.getText();
+			System.out.println(label);
+		}
+		driver.quit();
+	}
+	
+	@Test
+	public void Login()
+	{
+	    String userName, passWord;
+	    String actualUrl,expectedUrl;
+	    WebElement username,password,submitBtn;
+	    //wait = new WebDriverWait(driver,5);
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
+		//<input type="text" name="sid" size="11" maxlength="9" id="UserID">
+		username = driver.findElement(By.name("sid"));
+		//Instead of read just enter your username and password
+		userName = "";
+		username.sendKeys(userName);
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("PIN")));
+		//<input type="password" name="PIN" size="31" maxlength="30">
+		password = driver.findElement(By.name("PIN"));
+		passWord = "";
+		password.sendKeys(passWord);
+		//<input type="submit" value="Login">	    
+		//wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='submit']")));
+		submitBtn = driver.findElement(By.cssSelector("input[type='submit']"));
+		submitBtn.click();
+		//Added a bit of checking in case you entered the wrong values
+		actualUrl ="https://swing.langara.bc.ca/prod/twbkwbis.P_GenMenu?"; 
+		expectedUrl = driver.getCurrentUrl();
+		Assert.assertTrue(expectedUrl.contains(actualUrl));
+	}
+
+	@AfterMethod
+	public void aftermethod()
+	{
+		
+	}
+}
---
+++ Course-Planning/Search-Nearby/addMarkers.js
@@ -0,0 +1,78 @@
+/*
+https://developers.google.com/maps/documentation/javascript/examples/marker-remove
+*/
+
+	function addMarker(latLng) 
+	{
+		marker = new google.maps.Marker
+		({
+			position: latLng,
+			title: 'Point A',
+			map: map,
+			icon:icon,
+			draggable: true
+		});
+		
+		// Add circle overlay and bind to marker.
+		//This is where you can visit and come back in 1 hour.
+		circle1	= new google.maps.Circle
+		({
+			map: map,
+			radius: radius1, 
+			strokeColor: '#33cc33',
+			strokeOpacity: 0.4,
+			strokeWeight: 2,	
+			fillOpacity: 0.15,		
+			fillColor: '#33cc33'
+		});
+		circle1.bindTo('center', marker, 'position');
+			
+		// This is the maximum spot you can go in 1 hour.
+		circle2 = new google.maps.Circle
+		({
+			map: map,
+			radius: radius2,
+			strokeColor: '#e67300',
+			strokeOpacity: 0.4,
+			strokeWeight: 2,
+			fillOpacity: 0.15,	
+			fillColor: '#e67300'
+		});
+		circle2.bindTo('center', marker, 'position');
+		
+		marker.circle1 = circle1;
+		marker.circle2 = circle2;
+		markers.push(marker);
+	
+ 	};
+
+	// Removes the markers from the map, but keeps them in the array.
+	function clearMarkers() 
+	{
+       	 	setMapOnAll(null);
+    	};
+	
+	function setMapOnAll(map)
+	{
+       		for (var i = 0; i < markers.length; i++) 
+		{
+			markers[i].setMap(map);
+			markers[i].circle1.unbindAll();
+			markers[i].circle1.setMap(null);
+			markers[i].circle2.unbindAll();
+			markers[i].circle2.setMap(null);
+       		}
+    	};
+	
+	function showMarkers() 
+	{
+		setMapOnAll(map);
+    	};
+
+      // Deletes all markers in the array by removing references to them.
+	function deleteMarkers() 
+	{
+		clearMarkers();
+        	markers = [];
+    	};
+	  
