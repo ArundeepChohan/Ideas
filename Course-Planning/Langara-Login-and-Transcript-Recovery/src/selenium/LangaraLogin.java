5c653795129bd10b30e3ed9ff62e11f0fe7ef8a5
Arundeep Singh Chohan <arundeepchohan2009@hotmail.com>
2020-01-21 04:09:39 -0800

    Update LangaraLogin.java

--- Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
+++ Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
@@ -17,10 +17,9 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class LangaraLogin {
-	
 	WebDriver driver = null;
 	WebDriverWait wait = null;
-    String actualUrl, expectedUrl;
+    	String actualUrl, expectedUrl;
 	
 	@Parameters("browser")
 	@BeforeClass
@@ -37,19 +36,19 @@ public class LangaraLogin {
 	    }
 		driver.manage().window().maximize();
 		actualUrl = "https://swing.langara.bc.ca/prod/twbkwbis.P_WWWLogin"; 
-	    driver.get(actualUrl);
+	    	driver.get(actualUrl);
 		expectedUrl = driver.getCurrentUrl();
 		Assert.assertTrue(expectedUrl.equals(actualUrl));
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+	    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	}	
 	@Test(priority = 1)
 	public void Login()
 	{
-	    String userName, passWord;
-	    WebElement username, password, submitBtn;
-	    wait = new WebDriverWait(driver,2);
+	    	String userName, passWord;
+	    	WebElement username, password, submitBtn;
+	    	wait = new WebDriverWait(driver,2);
 		//<input type="text" name="sid" size="11" maxlength="9" id="UserID">
-	    username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
+	    	username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
 		//Enter your username and password
 		userName = "";
 		username.clear();
@@ -75,19 +74,19 @@ public class LangaraLogin {
 		wait = new WebDriverWait(driver,2);
 		//<a href="/prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu" onmouseover="window.status='Students'; return true" onmouseout="window.status=''; return true" onfocus="window.status='Students'; return true" onblur="window.status=''; return true">Students</a>
 		WebElement students = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Students")));
-	    students.click();
-	    WebElement studentrecords = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
-	    studentrecords.click();
-	    WebElement viewyourtranscript = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Your Transcript")));
-	    viewyourtranscript.click();
-	    ///html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody
-	    //WebElement transcript = driver.findElement(By.xpath("/html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody"));
-	    //System.out.println(transcript.getText());
-	    //Would like to pull out the information and store it into a class with Subject, No, Title, Credit, Grade, GPA. Remove the last column as well.
-	    //<td class="dddefault">CPSC</td>
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("dddefault")));
-	    List<WebElement> courses = driver.findElements(By.className("dddefault"));
-	    Iterator<WebElement> iter = courses.iterator();
+	    	students.click();
+	   	WebElement studentrecords = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
+	    	studentrecords.click();
+	    	WebElement viewyourtranscript = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Your Transcript")));
+	    	viewyourtranscript.click();
+	    	///html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody
+	    	//WebElement transcript = driver.findElement(By.xpath("/html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody"));
+	    	//System.out.println(transcript.getText());
+	    	//Would like to pull out the information and store it into a class with Subject, No, Title, Credit, Grade, GPA. Remove the last column as well.
+	    	//<td class="dddefault">CPSC</td>
+	    	wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("dddefault")));
+	    	List<WebElement> courses = driver.findElements(By.className("dddefault"));
+	    	Iterator<WebElement> iter = courses.iterator();
 		// This will check whether list has some element or not
 		while (iter.hasNext()) {
 			WebElement item = iter.next();
