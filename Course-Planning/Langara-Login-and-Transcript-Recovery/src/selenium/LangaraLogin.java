43531c8ec3e45659777c28ee4412648f28f03ba0
Arundeep Singh Chohan <arundeepchohan2009@hotmail.com>
2020-01-25 23:37:11 -0800

    Add files via upload

--- Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
+++ Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,45 +18,56 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class LangaraLogin {
+	
 	WebDriver driver = null;
 	WebDriverWait wait = null;
-    	String actualUrl, expectedUrl;
+    String actualUrl, expectedUrl;
 	
 	@Parameters("browser")
 	@BeforeClass
 	public void BeforeClass(String browser) 
 	{
-		if(browser.equalsIgnoreCase("google chrome")) {
-			System.setProperty("webdriver.chrome.driver", ".\\BrowserDrivers\\chromedriver_win32\\chromedriver.exe");
-			driver = new ChromeDriver();  
-		}else if (browser.equalsIgnoreCase("ie")) { 
-			System.setProperty("webdriver.ie.driver", ".\\BrowserDrivers\\IEDriverServer_Win32_3.150.1\\IEDriverServer.exe");
-			driver = new InternetExplorerDriver();;
-		} else {
-	        System.out.println("The Browser Type is Undefined");
-	    }
+		switch(browser.toLowerCase())
+		{
+			case "google chrome":
+				System.setProperty("webdriver.chrome.driver", ".\\BrowserDrivers\\chromedriver_win32\\chromedriver.exe");
+				driver = new ChromeDriver(); 
+				break;
+			case "firefox":
+				System.setProperty("webdriver.gecko.driver", ".\\BrowserDrivers\\geckodriver-v0.26.0-win32\\geckodriver.exe");
+				driver = new FirefoxDriver();
+			case "ie":
+				System.setProperty("webdriver.ie.driver", ".\\BrowserDrivers\\iedriverserver_win32_3.150.1\\iedriverserver.exe");
+				driver = new InternetExplorerDriver();
+				break;
+			default:
+				System.out.println("The Browser Type is Undefined.");
+				break;
+		}
+		
 		driver.manage().window().maximize();
 		actualUrl = "https://swing.langara.bc.ca/prod/twbkwbis.P_WWWLogin"; 
-	    	driver.get(actualUrl);
+	    driver.get(actualUrl);
 		expectedUrl = driver.getCurrentUrl();
 		Assert.assertTrue(expectedUrl.equals(actualUrl));
-	    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+		//System.out.println("Am I at the login:"+expectedUrl.equals(actualUrl));
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	}	
 	@Test(priority = 1)
 	public void Login()
 	{
-	    	String userName, passWord;
-	    	WebElement username, password, submitBtn;
-	    	wait = new WebDriverWait(driver,2);
+	    String userName, passWord;
+	    WebElement username, password, submitBtn;
+	    wait = new WebDriverWait(driver,2);
 		//<input type="text" name="sid" size="11" maxlength="9" id="UserID">
-	    	username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
+	    username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
 		//Enter your username and password
-		userName = "";
+		userName = "100201711";
 		username.clear();
 		username.sendKeys(userName);
 		//<input type="password" name="PIN" size="31" maxlength="30">
 		password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("PIN")));
-		passWord = "";
+		passWord = "Ac-19920330";
 		password.clear();
 		password.sendKeys(passWord);
 		//<input type="submit" value="Login">	    
@@ -65,27 +77,26 @@ public class LangaraLogin {
 		actualUrl = "https://swing.langara.bc.ca/prod/twbkwbis.P_GenMenu?"; 
 		expectedUrl = driver.getCurrentUrl();
 		Assert.assertTrue(expectedUrl.contains(actualUrl));
+		//System.out.println("Did I login:"+expectedUrl.contains(actualUrl));
 	}
 	
 	@Test(priority = 2)
 	public void PullTranscripts()
 	{
-		wait = new WebDriverWait(driver,2);
 		//<a href="/prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu" onmouseover="window.status='Students'; return true" onmouseout="window.status=''; return true" onfocus="window.status='Students'; return true" onblur="window.status=''; return true">Students</a>
 		WebElement students = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Students")));
-	    	students.click();
-	   	WebElement studentrecords = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
-	    	studentrecords.click();
-	    	WebElement viewyourtranscript = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Your Transcript")));
-	    	viewyourtranscript.click();
-	    	///html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody
-	    	//WebElement transcript = driver.findElement(By.xpath("/html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody"));
-	    	//System.out.println(transcript.getText());
-	    	//Would like to pull out the information and store it into a class with Subject, No, Title, Credit, Grade, GPA. Remove the last column as well.
-	    	//<td class="dddefault">CPSC</td>
-	    	wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("dddefault")));
-	    	List<WebElement> courses = driver.findElements(By.className("dddefault"));
-	    	Iterator<WebElement> iter = courses.iterator();
+	    students.click();
+	    WebElement studentrecords = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
+	    studentrecords.click();
+	    WebElement viewyourtranscript = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Your Transcript")));
+	    viewyourtranscript.click();
+	    ///html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody
+	    //WebElement transcript = driver.findElement(By.xpath("/html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody"));
+	    //System.out.println(transcript.getText());
+	    //Would like to pull out the information and store it into a class with Subject, No, Title, Credit, Grade, GPA. Remove the last column as well.
+	    //<td class="dddefault">CPSC</td>
+	    List<WebElement> courses = driver.findElements(By.className("dddefault"));
+	    Iterator<WebElement> iter = courses.iterator();
 		// This will check whether list has some element or not
 		while (iter.hasNext()) {
 			WebElement item = iter.next();
