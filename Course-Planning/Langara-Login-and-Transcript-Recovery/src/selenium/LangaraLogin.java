8fabce52dbb545ec16eb835473d81f07db6e52e9
Arundeep Singh Chohan <arundeepchohan2009@hotmail.com>
2020-01-31 00:27:17 -0800

    Update LangaraLogin.java

--- Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
+++ Course-Planning/Langara-Login-and-Transcript-Recovery/src/selenium/LangaraLogin.java
@@ -22,7 +22,7 @@ public class LangaraLogin {
 	
 	WebDriver driver = null;
 	WebDriverWait wait = null;
-    String actualUrl, expectedUrl;
+    	String actualUrl, expectedUrl;
 	
 	@Parameters("browser")
 	@BeforeClass
@@ -51,33 +51,33 @@ public class LangaraLogin {
 		
 		driver.manage().window().maximize();
 		actualUrl = "https://swing.langara.bc.ca/prod/twbkwbis.P_WWWLogin"; 
-	    driver.get(actualUrl);
+	    	driver.get(actualUrl);
 		//System.out.println("Am I at the login:"+expectedUrl.equals(actualUrl));
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		expectedUrl = driver.getCurrentUrl();
 		Assert.assertTrue(expectedUrl.equals(actualUrl)); 
 	}	
 	@Test(priority = 1)
 	public void Login()
 	{
-	    String userName, passWord;
-	    WebElement username, password, submitBtn;
-	    wait = new WebDriverWait(driver,5);
+	    	String userName, passWord;
+	    	WebElement username, password, submitBtn;
+	    	wait = new WebDriverWait(driver,5);
 		//<input type="text" name="sid" size="11" maxlength="9" id="UserID">
-	    username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
+	    	username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("sid")));
 		//Enter your username and password
-		userName = "100201711";
+		userName = "";
 		username.clear();
 		username.sendKeys(userName);
 		//<input type="password" name="PIN" size="31" maxlength="30">
 		password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("PIN")));
-		passWord = "Ac-19920330";
+		passWord = "";
 		password.clear();
 		password.sendKeys(passWord);
 		//<input type="submit" value="Login">	    
 		submitBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='submit']")));
 		submitBtn.click();
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		//Added a bit of checking in case you entered the wrong values
 		actualUrl = "https://swing.langara.bc.ca/prod/twbkwbis.P_GenMenu?"; 
 		expectedUrl = driver.getCurrentUrl();
@@ -90,19 +90,19 @@ public class LangaraLogin {
 	{
 		//<a href="/prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu" onmouseover="window.status='Students'; return true" onmouseout="window.status=''; return true" onfocus="window.status='Students'; return true" onblur="window.status=''; return true">Students</a>
 		WebElement students = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Students")));
-	    students.click();
-	    WebElement studentrecords = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
-	    studentrecords.click();
-	    WebElement viewyourtranscript = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Your Transcript")));
-	    viewyourtranscript.click();
-	    ///html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody
-	    //WebElement transcript = driver.findElement(By.xpath("/html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody"));
-	    //System.out.println(transcript.getText());
-	    //Would like to pull out the information and store it into a class with Subject, No, Title, Credit, Grade, GPA. Remove the last column as well.
-	    //<td class="dddefault">CPSC</td>
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    List<WebElement> courses = driver.findElements(By.className("dddefault"));
-	    Iterator<WebElement> iter = courses.iterator();
+	    	students.click();
+	    	WebElement studentrecords = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Student Records")));
+	    	studentrecords.click();
+	    	WebElement viewyourtranscript = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Your Transcript")));
+	    	viewyourtranscript.click();
+	    	///html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody
+	    	//WebElement transcript = driver.findElement(By.xpath("/html/body/p/table/tbody/tr/td[2]/div[2]/p/table[1]/tbody"));
+	    	//System.out.println(transcript.getText());
+	    	//Would like to pull out the information and store it into a class with Subject, No, Title, Credit, Grade, GPA. Remove the last column as well.
+	    	//<td class="dddefault">CPSC</td>
+	    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    	List<WebElement> courses = driver.findElements(By.className("dddefault"));
+	    	Iterator<WebElement> iter = courses.iterator();
 		// This will check whether list has some element or not
 		while (iter.hasNext()) {
 			WebElement item = iter.next();
